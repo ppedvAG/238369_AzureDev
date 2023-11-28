04412c1b83faea97f2d163be7b64e7cd703e7ace
André <AndreR@ppedv.de>
2023-11-28 16:37:45 +0100

    DocumentAi (Forms recognizer) consolen app

---
+++ DocumentAiClient/DocumentAiClient/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure;
+using Azure.AI.FormRecognizer;
+using Azure.AI.FormRecognizer.DocumentAnalysis;
+
+Console.WriteLine("Hello, World!");
+
+var endPoint = "https://mydocaaaaaaa.cognitiveservices.azure.com/";
+var key = "a9e8644a3f974d30a910f70087bacfc9";
+
+FormRecognizerClient client = new FormRecognizerClient(new Uri(endPoint), new AzureKeyCredential(key));
+
+var docClient = new DocumentAnalysisClient(new Uri(endPoint), new AzureKeyCredential(key));
+
+var docPath = @"C:\Users\Fred\Desktop\HolzRECHNUNG7\HolzRECHNUNG7.pdf";
+var result = docClient.AnalyzeDocument(WaitUntil.Completed, "cccc", File.OpenRead(docPath));
+
+var summe = result.Value.Documents.First().Fields["Summe"].Content;
+var re = result.Value.Documents.First().Fields["Rechnungsempfaenger"].Content;
+
+Console.WriteLine(summe);
+Console.WriteLine(re);
