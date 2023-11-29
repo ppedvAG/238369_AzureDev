cfd8dcb158c3ab177ee028912afb6443a663b275
André <AndreR@ppedv.de>
2023-11-29 09:51:10 +0100

    Books mit EF

---
+++ HalloBlazorServer/HalloBlazorServer/HalloBlazorServer/Data/BooksContext.cs
@@ -0,0 +1,17 @@
+﻿using HalloBlazorServer.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace HalloBlazorServer.Data
+{
+    public class BooksContext:DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Books_dev;Trusted_Connection=true;");
+        }
+
+    }
+}
---
+++ HalloBlazorServer/HalloBlazorServer/HalloBlazorServer/Model/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace HalloBlazorServer.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Subtile { get; set; } = string.Empty;
+        public int PageCount { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public virtual ICollection<Author> Authors { get; set; } = new HashSet<Author>();
+    }
+}
