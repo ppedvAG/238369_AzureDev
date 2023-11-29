c2a4d746e66cbafc1d1a95f66759dc09bc56e515
André <AndreR@ppedv.de>
2023-11-29 16:12:02 +0100

    Pizza

---
+++ HalloBlazorServer/HalloBlazorServer/HalloBlazorServer/Model/Pizza.cs
@@ -0,0 +1,13 @@
+﻿namespace HalloBlazorServer.Model
+{
+
+    public class Pizza
+    {
+        public string id { get; set; }
+        public string Name { get; set; }
+        public decimal Preis { get; set; }
+        public int Kcal { get; set; }
+
+    }
+
+}
